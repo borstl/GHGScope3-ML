6b3aa32327ec1cbdfd16800e31648fb8cea3067a
Sebastian Becker <sebbec@posteo.de>
2025-07-28 20:58:56 +0200

    filtering out valid static fields and reformat code

--- src/functions/download.py
+++ src/functions/download.py
@@ -14,14 +14,15 @@ from functions.modeling import join_static_and_historic, concat_companies
 COMPANIES_PATH: str = "../data/parameter/companies.txt"
 STATIC_FIELDS_PATH: str = "../data/parameter/tr_values_static.txt"
 TIME_SERIES_FIELDS_PATH: str = "../data/parameter/tr_values_history.txt"
+# SKIP: int = 22_800
 PARAMS: dict = {
     "SDate": "CY2010",
     "EDate": "CY2024",
     "Period": "FY0",
-    "Frq": "CY" # Yearly frequency
+    "Frq": "CY"  # Yearly frequency
 }
-CHUNK_SIZE: int = 10
-CHUNK_LIMIT: int = 3
+CHUNK_SIZE: int = 100
+CHUNK_LIMIT: int | None = None
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -33,36 +34,40 @@ def download_content(companies_path, static_fields_path, time_series_fields_path
     with open(static_fields_path, encoding="utf-8") as f:
         static_fields: list[str] = [line.strip() for line in f]
     with open(time_series_fields_path, encoding="utf-8") as f:
-        time_series_fields: list[str] = [line.strip() for line in f]
+        historic_fields: list[str] = [line.strip() for line in f]
+    download_all_frames(companies, static_fields, historic_fields)
 
+
+def download_all_frames(companies, static_fields, historic_fields):
+    """Downloading all frames from LSEG database"""
     full_dataframe: DataFrame = pd.DataFrame()
     ld.open_session()
     for company in companies:
-        print("Downloading static of " + company)
-        static_dataframe = download_all_static_chunks(company, static_fields)
-
-        print("Downloading time series of " + company)
-        historic_dataframe = download_all_time_series_chunks(company, time_series_fields)
-
-        company_frame = join_static_and_historic(static_dataframe, historic_dataframe)
-
-        if full_dataframe.empty:
-            date_frame = pd.DataFrame(company_frame.index.to_series(), columns=['Date'])
-            company_frame.insert(0, 'Date', date_frame)
-            full_dataframe = company_frame
-        else:
-            debug_dataframe = concat_companies(full_dataframe, company_frame)
-            full_dataframe = debug_dataframe
-
-        # reduced_df = remove_empty_columns(full_df)
-        # full_dataframe.to_csv("../data/datasets/" + company + ".csv", index_label="Date")
+        static: DataFrame = download_all_static_chunks(company, static_fields)
+        # included_fields: list[str] = static_fields[SKIP:]
+        # static_dataframe = download_all_static_chunks(company, included_fields)
+        # static_dataframe.to_csv("../data/datasets/static/" + company + ".csv")
+        historic: DataFrame = download_all_time_series_chunks(company, historic_fields)
+        company_dataframe: DataFrame = join_static_and_historic(static, historic)
+        full_dataframe = bundle(full_dataframe, company_dataframe)
     ld.close_session()
 
 
-def split_in_chunks(field_list: list[str],
-                    chunk_size:int = CHUNK_SIZE,
-                    chunk_limit: int | None = None) -> list[list]:
-    """Splitting a list in chunks of 1000 items"""
+def bundle(new_dataframe: DataFrame, old_dataframe: DataFrame) -> DataFrame:
+    """Concatenate two company dataframes if there is already one filled with data"""
+    if old_dataframe.empty:
+        date_frame = pd.DataFrame(new_dataframe.index.to_series(), columns=['Date'])
+        new_dataframe.insert(0, 'Date', date_frame)
+        return new_dataframe
+    return concat_companies(old_dataframe, new_dataframe)
+
+
+def split_in_chunks(
+        field_list: list[str],
+        chunk_size: int = CHUNK_SIZE,
+        chunk_limit: int | None = None
+) -> list[list]:
+    """Splitting a list in chunks of e.g. 1000 items"""
     chunks: list[list] = []
     for i in range(0, len(field_list), chunk_size):
         chunks.append(field_list[i: i + chunk_size])
@@ -73,6 +78,7 @@ def split_in_chunks(field_list: list[str],
 
 def download_all_static_chunks(company: str, fields: list[str]) -> DataFrame:
     """Downloading all static fields from a company"""
+    print("Downloading static of " + company)
     chunks: list[list] = split_in_chunks(fields, CHUNK_SIZE, CHUNK_LIMIT)
     dataframe: DataFrame = pd.DataFrame()
     for chunk in progressbar.progressbar(chunks):
@@ -91,6 +97,7 @@ def download_all_static_chunks(company: str, fields: list[str]) -> DataFrame:
 
 def download_all_time_series_chunks(company: str, fields: list[str]) -> DataFrame:
     """Downloading all fields from a company and join them together"""
+    print("Downloading time series of " + company)
     chunks: list[list] = split_in_chunks(fields, CHUNK_SIZE, CHUNK_LIMIT)
     dataframe: DataFrame = pd.DataFrame()
     for chunk in progressbar.progressbar(chunks):
@@ -136,6 +143,15 @@ def download_gics_codes():
     )
     ld.close_session()
     gics_codes.to_csv("../data/datasets/gics_industry_codes.csv")
+
+
+def configure_lseg():
+    """Configure LSEG"""
+    config = ld.get_config()
+    config.set_param("logs.transports.console.enabled", True)
+    config.set_param("logs.level", "debug")
+    config.set_param("logs.transports.file.name", "lseg-data-lib.log")
+    config.set_param("http.request-timeout", 600_000)
 
 
 if __name__ == "__main__":
