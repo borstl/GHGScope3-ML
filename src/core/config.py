573e59e7113d2b90641266303abe8983b2669e6a
Sebastian Becker <sebbec@posteo.de>
2026-02-16 12:57:27 +0100

    history median/mode imputation

--- src/core/config.py
+++ src/core/config.py
@@ -57,6 +57,10 @@ class Config:
     eda_features_file_static: Path = features_dir / "eda_filtered_static_featuresA-Z.txt"
     eda_features_file_historic: Path = features_dir / "eda_filtered_time_series_featuresA-Z.txt"
 
+    baseline_dir: Path = dataset_dir / "baseline"
+    baseline_static: Path = baseline_dir / "baseline_imputed_static.csv"
+    baseline_historic: Path = baseline_dir / "baseline_imputed_historic.csv"
+
     full_dir: Path = dataset_dir / "full"
     full_dir_raw_data: Path = full_dir / "full_raw"
     full_dir_static: Path = full_dir / "static"
